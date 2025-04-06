48ad1a62b2578740b2eab853aa2c0b4cef59c0e8
Aaru5h <mestrikerocks@gmail.com>
2025-04-06 15:37:47 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+// for(let i = 0; i<document.querySelectorAll('.drum').length; i++){
+
+// }
+
+document.getElementsByClassName('drum').addEventListener("click",handleClick)
+
+function handleClick(){
+    alert('Clicked');
+
+}
